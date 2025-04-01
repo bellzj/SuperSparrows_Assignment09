a204339fe68e2b1ca0ae0d979531514bbeecb327
Zach Bell <bellzj@mail.uc.edu>
2025-04-01 16:00:46 -0400

    Questions 1-4 and edits to main.py

--- SuperSparrows_Assignment09/dataPackage/Zach.py
+++ SuperSparrows_Assignment09/dataPackage/Zach.py
@@ -1,1 +1,45 @@
+#Zach.py
+import pyodbc
+import random
+
+def connect_to_database():
+    """Connect to SQL Server instance and return connection object."""
+    conn = pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+        'Database=GroceryStoreSimulator;'
+        'uid=IS4010Login;'
+        'pwd=P@ssword2;'
+    )
+    return conn
+
+def fetch_products():
+    """Fetch product data from tProduct and return a list of dictionaries."""
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    query = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID FROM tProduct"
+    cursor.execute(query)
+
+    columns = [column[0] for column in cursor.description]
+    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
+    return results
+
+def fetch_manufacturer_name(manufacturer_id):
+    """Fetch Manufacturer name using ManufacturerID and return the name."""
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    query = f"SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = {manufacturer_id}"
+    cursor.execute(query)
+
+    manufacturer = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return manufacturer[0] if manufacturer else None
 
--- SuperSparrows_Assignment09/mainPackage/main.py
+++ SuperSparrows_Assignment09/mainPackage/main.py
@@ -1,1 +1,21 @@
+#main.py
+
+from dataPackage import Zach
+import random
+
+# Fetch product data
+products = Zach.fetch_products()
+
+if products:
+    # Randomly select a product
+    selected_product = random.choice(products)
+
+    # Store values in variables
+    product_id = selected_product["ProductID"]
+    description = selected_product["Description"]
+    manufacturer_id = selected_product["ManufacturerID"]
+    brand_id = selected_product["BrandID"]
+
+    # Fetch Manufacturer Name
+    manufacturer_name = Zach.fetch_manufacturer_name(manufacturer_id)
 
